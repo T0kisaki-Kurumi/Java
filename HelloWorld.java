69e651cb1d120ff2f1570670bc436722ed2d01b6
Robbie <875241454@qq.com>
2024-08-30 09:41:35 +0800

    helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 /**
- * @author robbie
- * &#064;date  Created in 2024/08/30
+ * @author Robbie
+ * @since 2024/08/30
  */
 
-public class Helloworld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
