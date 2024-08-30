ba6c4d75338d7d56f0df6cac6359a7cb3a25772e
Robbie <875241454@qq.com>
2024-08-30 09:21:43 +0800

    helloworld

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+/**
+ * @author robbie
+ * &#064;date  Created in 2024/08/30
+ */
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
